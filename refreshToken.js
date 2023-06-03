64b502a7903e3ffb53705d825cd755be55ad522f
Zwiqler94 <10226084+Zwiqler94@users.noreply.github.com>
2023-06-03 13:30:42 -0400

    update secret name

--- refreshToken.js
+++ refreshToken.js
@@ -1,21 +1,26 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 async function run() {
-    try {
-        const octo = github.getOctokit({ token: await core.getIDToken('public_repo') });
-        const s =  await octo.request('GET /repos/{owner}/{repo}/actions/secrets/{secret_name}', {
-            owner: 'Zwiqler94',
-            repo: 'jz-portfolio',
-            secret_name: 'APP_CHECK_TOKEN',
-            headers: {
-                'X-GitHub-Api-Version': '2022-11-28'
-            }
-        });
-        console.log(s);
-    } catch (error) {
-        core.setFailed(error.message);
-    }
+  try {
+    const token = await core.getIDToken("public_repo");
+    core.info(token);
+    const octo = github.getOctokit({ token });
+    const s = await octo.request(
+      "GET /repos/{owner}/{repo}/actions/secrets/{secret_name}",
+      {
+        owner: "Zwiqler94",
+        repo: "jz-portfolio",
+        secret_name: "APP_ACCESS_TOKEN",
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      }
+    );
+    core.info(s);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run();
